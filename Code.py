e6a0bd460edc6377d056b5ca3e7cb5b0aa52323a
Vinay Bhati <68839268+vinay-bhati@users.noreply.github.com>
2024-10-29 18:48:38 -0700

    Update Code.py

--- Code.py
+++ Code.py
@@ -275,7 +275,7 @@ if email:
                     with col1:
                         age = st.number_input("Enter Age (Years):", min_value=3, max_value=95, step=1, key='age')
                     with col2:
-                        height = st.number_input("Enter Height (in cm):", min_value=150, max_value=250, step=1, key='height')
+                        height = st.number_input("Enter Height (in cm):", min_value=150, max_value=250, step=1, key='height',format="%.2f")
                     with col3:
                         measured_fev1 = st.number_input("Enter Measured FEV1 (XX.XX):", min_value=0.0, format="%.2f", step=0.01, key='fev1')
                     with col4:
@@ -349,7 +349,7 @@ if email:
                     with col1:
                         age = st.number_input("Age:", min_value=3, max_value=95, step=1)
                     with col2:
-                        height = st.number_input("Height (in cm):", min_value=100, max_value=250, step=1)
+                        height = st.number_input("Height (in cm):", min_value=100, max_value=250, step=1,format="%.1f")
                     with col3:
                         measured_fev1 = st.number_input("Enter Measured FEV1 (XX.XX):", min_value=0.0, format="%.2f", step=0.01)
                     with col4:
