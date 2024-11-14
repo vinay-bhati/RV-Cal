a5135cdde795810667118d814ae5fcac94aa2184
Vinay Bhati <68839268+vinay-bhati@users.noreply.github.com>
2024-11-14 15:29:14 -0800

    Update Code.py

--- Code.py
+++ Code.py
@@ -371,20 +371,20 @@ if email:
                         pred_fvc = calculate_ecsc_fvc(age, height, measured_fev1, measured_fvc, gender, race)
                         fvc_percent_predicted_ecsc, fev1_fvc_ratio, rv_percent_est, rv150, rv175, rv200 = calculate_ecsc_metrics(age, height, measured_fev1, pred_fvc, measured_fvc)
 
-                        col5, col6, col7, col8,col9, col10, col11 = st.columns(7)
+                        '''col5, col6, col7,''' col8, col9, col10, col11 = st.columns(7)
                         # Display the calculated values
-                        with col5:
-                            st.metric(label="Predicted FVC:", value=f"{pred_fvc}")
-                            #st.write(f"Predicted FVC: {pred_fvc} L")
-                        with col6:
-                            st.metric(label="FVC % Predicted:", value=f"{fvc_percent_predicted_ecsc}")
-                            #st.write(f"FVC % Predicted: {fvc_percent_predicted_ecsc}%")
-                        with col7:
-                            st.metric(label="FEV1/FVC Ratio:", value=f"{fev1_fvc_ratio}")
-                            #st.write(f"FEV1/FVC Ratio: {fev1_fvc_ratio}%")
-                        with col8:    
-                            st.metric(label="RV % Estimated:", value=f"{rv_percent_est}")
-                            #st.write(f"RV % Estimated: {rv_percent_est}")
+                        # with col5:
+                        #     st.metric(label="Predicted FVC:", value=f"{pred_fvc}")
+                        #     #st.write(f"Predicted FVC: {pred_fvc} L")
+                        # with col6:
+                        #     st.metric(label="FVC % Predicted:", value=f"{fvc_percent_predicted_ecsc}")
+                        #     #st.write(f"FVC % Predicted: {fvc_percent_predicted_ecsc}%")
+                        # with col7:
+                        #     st.metric(label="FEV1/FVC Ratio:", value=f"{fev1_fvc_ratio}")
+                        #     #st.write(f"FEV1/FVC Ratio: {fev1_fvc_ratio}%")
+                        # with col8:    
+                        #     st.metric(label="RV % Estimated:", value=f"{rv_percent_est}")
+                        #     #st.write(f"RV % Estimated: {rv_percent_est}")
                         with col9:
                             st.metric(label="RV >150% Probability", value=f"{rv150}%")
                             #st.write(f"RV >150% Probability: {rv150}%")
